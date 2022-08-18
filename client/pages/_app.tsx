11c81c7363e0849ce390e08623e8c7b46600de54
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-18 11:00:07 +0900

    update

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../context/theme'
+import { TopNav } from '../components/TopNav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <TopNav />
       <Component {...pageProps} />
     </ThemeProvider>
   )
